7a50e2e1b53a49de92b2f297d082ef2bd66c9e50
abdure-ai <abdurewzebtu143@gmail.com>
2026-03-28 17:05:49 +0300

    Deploy: finalize local changes for cPanel deployment

--- backend_old/src/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-const { authenticateToken, authorizeRole } = require('../middleware/authMiddleware');
-
-router.post('/login', authController.login);
-router.post('/register', authenticateToken, authorizeRole(['super_admin']), authController.register);
-
-module.exports = router;
--- backend_old/src/routes/contactRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const contactController = require('../controllers/contactController');
-
-router.post('/', contactController.submitMessage);
-router.get('/', contactController.getMessages); // For admin use
-
-module.exports = router;
